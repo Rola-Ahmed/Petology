bc3a15c644df651c0f38369fbb398c1807a338ff
Rola-Ahmed <75480678+Rola-Ahmed@users.noreply.github.com>
2022-07-27 22:44:58 +0200

    Update sign_up.dart

--- lib/view/sign_up.dart
+++ lib/view/sign_up.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 
 import '../responsive.dart';
 import '../top_bar_contents.dart';
-import  "package:petology/font/costum_google_icon.dart";
+import "package:petology/font/costum_google_icon.dart";
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+
 
 class SignUp extends StatefulWidget {
   @override
@@ -22,137 +24,170 @@ class _SignUpState extends State<SignUp> {
               preferredSize: Size(screenSize.width, 70),
               child: TopBarContents(),
             ),
-      body: Container(
-        margin: const EdgeInsets.all(30.0),
-        padding: const EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 1,
-            color: Colors.black,
+      body: Center(
+        child: Container(
+          width: 700,
+          margin: const EdgeInsets.all(30.0),
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.black,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(30), // POINT
+            ),
           ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(30), // POINT
-          ),
-        ),
-        child: Column(
-          children: [
-            Center(
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
+          child: Column(
+            children: [
+              Center(
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
                     color: Colors.brown,
                     fontWeight: FontWeight.bold,
-                    fontSize: 16),
-              ),
-            ),
-
-            ///////////////////////////////
-            
-         //   Column(
-           //   children: [
-                //Container(
-                 // padding:EdgeInsets.all(15),
-                //  child:
-                   Row(
-                    children: [
-                      Container(
-                        //color: Colors.blue,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          border: Border.all(
-                            width: 1,
-                            color: Colors.blue,
-                          ),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(30), // POINT
-                          ),
-                        ),
-
-                        child: Row(
-                          children: [
-
-                            IconButton(
-                              onPressed: () {
-                                //action coe when button is pressed
-                              },
-                              icon: Icon(Icons.facebook, color: Colors.white),
-                            ),
-                            Text(
-                              'Facebook',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16),
-                            ),
-                            SizedBox(  width: screenSize.width / 4,),
-                          ],
-                        ), 
-                      ),
-                      ////////////////////////////////////////////////////////////////////////
-                      //SizedBox(  width: screenSize.width / 4,),
-
-                      Container(
-                        //color: Colors.blue,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          border: Border.all(
-                            width: 1,
-                            color: Colors.blue,
-                          ),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(30), // POINT
-                          ),
-                        ),
-
-                        child: Row(
-                          children: [
-
-                           IconButton(
-                              onPressed: () {
-                                //action coe when button is pressed
-                              },
-                              icon: new Icon(MdiIcons.google,color: Colors.white,),
-                              iconSize: 50,hoverColor: Colors.white,
-                            //  splashColor: Colors.pink,
-                            ),
-                            Text(
-                              'Google',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16),
-                            ),
-
-                             
-
-                            
-                          ],
-                        ), 
-                      ),
-
-                    ],
+                    fontSize: 20,
                   ),
-               // ),
-          //    ],
-          ///  ),
+                ),
+              ),
+               SizedBox(height: 30),
 
-      //       ListTile(
-      // //  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      //   leading: Container(
-      //     child: Icon(Icons.autorenew, color: Colors.white),
-      //   ),
-      //   title: Text(
-      //     "Introduction to Driving",
-      //   ),
-      //   subtitle: Row(
-      //     children: <Widget>[
-      //       Icon(Icons.linear_scale, color: Colors.yellowAccent),
-      //       Text(" Intermediate", style: TextStyle(color: Colors.white))
-      //     ],
-      //   ),
-    
-      //  ),
-          ],
+              ///////////////////////////////
+
+              //   Column(
+              //   children: [
+              //Container(
+              // padding:EdgeInsets.all(15),
+              //  child:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                 Container(
+                    //color: Colors.blue,
+                    width: screenSize.width / 5,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30), // POINT
+                      ),
+                    ),
+
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue, //background color of button
+                        side: BorderSide(
+                            width: 3,
+                            
+                            color: Colors.blue), //border width and color
+                        elevation: 3, //elevation of button
+                        shape: RoundedRectangleBorder(
+                            //to set border radius to button
+                            borderRadius: BorderRadius.circular(30)),
+                       /// padding:
+                          //  EdgeInsets.only(top), //content padding inside button
+                      ),
+                      onPressed: () {},
+                      icon: new Icon(
+                        Icons.facebook,
+                        color: Colors.white,
+                      ),
+                      label: Text(
+                        'Facebook',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  ////////////////////////////////////////////////////////////////////////
+                  SizedBox(width: 10),
+
+                  Container(
+                    //color: Colors.blue,
+                    width: screenSize.width / 5,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30), // POINT
+                      ),
+                    ),
+
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white, //background color of button
+                        side: BorderSide(
+                            width: 3,
+                            
+                            color: Colors.black), //border width and color
+                        elevation: 3, //elevation of button
+                        shape: RoundedRectangleBorder(
+                            //to set border radius to button
+                            borderRadius: BorderRadius.circular(30)),
+                       /// padding:
+                          //  EdgeInsets.only(top), //content padding inside button
+                      ),
+                      onPressed: () {},
+                      icon: new Icon(
+                        MdiIcons.google,
+                        color: Colors.black,
+                      ),
+                      label: Text(
+                        'Google',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ),
+                  ),
+                  ///////////////////////////////////
+                ],
+              ),
+              Row(children: [
+                Text('________________________________________________ or _________________________________________________'),
+              ],),
+      SizedBox(width: 20),
+
+               Container(
+                margin: ,
+                 child: TextField(
+                         
+                          //controller: nameTextController,
+                          textAlign: TextAlign.left,
+                        
+                          decoration: new InputDecoration(
+                           
+    enabledBorder:  OutlineInputBorder(
+      // width: 0.0 produces a thin "hairline" border
+       borderRadius: BorderRadius.circular(30.0),
+      borderSide: const BorderSide(color: Color.fromARGB(255, 217, 217, 217), width: 1.0),
+    ),
+    border: const OutlineInputBorder(),
+    labelStyle: new TextStyle(color: Color.fromARGB(255, 176, 124, 105),),
+   hintText: 'First Name',
+  ),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                           // height: 0.01,
+
+                            //width:
+                          ),
+                        ),
+               ),
+            ],
+          ),
         ),
       ),
     );
